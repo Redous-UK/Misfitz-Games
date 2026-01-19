57730af508d8c233b807ad93d4943325a75bb17f
Redous-UK <cb2122@gmail.com>
2026-01-19 02:41:41 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -20,13 +20,28 @@ public static class Program
             var redisUrl = builder.Configuration["REDIS_URL"]
                 ?? throw new InvalidOperationException("REDIS_URL not set");
 
-            var options = ConfigurationOptions.Parse(redisUrl, ignoreUnknown: true);
-            options.AbortOnConnectFail = false;
-            options.ConnectRetry = 10;
-            options.ConnectTimeout = 15000;
-            options.KeepAlive = 30;
+            var uri = new Uri(redisUrl);
 
-            return ConnectionMultiplexer.Connect(options);
+            var userInfo = uri.UserInfo.Split(':', 2);
+            var username = userInfo.Length > 0 ? Uri.UnescapeDataString(userInfo[0]) : "";
+            var password = userInfo.Length > 1 ? Uri.UnescapeDataString(userInfo[1]) : "";
+
+            var opts = new ConfigurationOptions
+            {
+                AbortOnConnectFail = false,
+                ConnectRetry = 10,
+                ConnectTimeout = 20000,
+                KeepAlive = 30,
+                Ssl = uri.Scheme.Equals("rediss", StringComparison.OrdinalIgnoreCase),
+                SslHost = uri.Host,          // important for SNI
+            };
+
+            opts.EndPoints.Add(uri.Host, uri.Port);
+
+            if (!string.IsNullOrWhiteSpace(password)) opts.Password = password;
+            if (!string.IsNullOrWhiteSpace(username)) opts.User = username;
+
+            return ConnectionMultiplexer.Connect(opts);
         });
 
 
