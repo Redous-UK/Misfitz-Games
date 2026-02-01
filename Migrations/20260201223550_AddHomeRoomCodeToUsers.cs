294fa91a63df7d5d0fcb0f92ce9d715a7db943c7
Redous-UK <cb2122@gmail.com>
2026-02-01 22:36:41 +0000

    added HomeRoomCode column to db.

---
+++ Migrations/20260201223550_AddHomeRoomCodeToUsers.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Misfitz_Games.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddHomeRoomCodeToUsers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "HomeRoomCode",
+                table: "Users",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "HomeRoomCode",
+                table: "Users");
+        }
+    }
+}
